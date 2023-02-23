939132dc38e4b3144a21d4af3bf6da1509d89886
diego-mories <diegomoriesmayo@usal.es>
2023-02-23 11:31:45 +0100

    Estructura de Back creada para empezar con base de datos

---
+++ vue-front/src/views/ConfigurationCommunity.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="screen">
+      <div class="row" id="grid-top-home">
+          <div class="col-sm-5" id="full">
+            <div id="img-container-home"></div>
+          </div>
+          <div class="col-sm-7" id="full">
+            <div class="row" id="topR">
+        <div class="col-sm-1" id="full">
+            <button class="btn btn-sm btn-primary" id="profileButton" @click="$router.push('/Profile')">Volver</button>
+        </div>
+        <div class="col-sm-11" id="full">
+            <span class="title"><b>Configuración comunidad</b></span>
+        </div>
+      </div>
+      <div class="row" id="bottomR">
+        <div class="col-sm-1" id="full">
+        </div>
+        <div class="col-sm-11" id="full">
+            <ConfigurationCommunityForm class="center-form"></ConfigurationCommunityForm>
+        </div>
+        </div>
+          </div>
+      </div>
+      <div class="row" id="grid-bottom-home"><FooterSocial id="full"></FooterSocial></div>
+    </div>
+  </template>
+
+<script>
+
+import FooterSocial from '../components/FooterSocialNetwork.vue'
+import ConfigurationCommunityForm from '../components/Forms/ConfigurationCommunityForm.vue'
+export default {
+  components: {
+    FooterSocial,
+    ConfigurationCommunityForm
+  }
+}
+</script>
+
+<style>
+  /* Extra */
+ #img-container-home{
+    width: 100%;
+    height: 100%;
+    background-image: url('../assets/images/comunidad.jpg');
+    background-size: cover;
+        -moz-background-size: cover;
+        -webkit-background-size: cover;
+        -o-background-size: cover;
+    opacity: 75%;
+  }
+</style>
