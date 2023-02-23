a871e314aaec4e5c61b8af332244f9dc13cc1bba
diego-mories <diegomoriesmayo@usal.es>
2023-02-23 19:42:24 +0100

    Proceso de servicio login

--- node-back/controllers/dataBaseServices.controller.js
+++ node-back/controllers/dataBaseServices.controller.js
@@ -15,12 +15,46 @@ conexion.connect((err, connection) => {
   }
 })
 
-// Testing
+// Testing y modelo de estructura de datos
 exports.test = (req,res) => {
-  console.log('AQUI TENEMOS EL CONTROLLER ' + req.query.email + res)
-  if (req.query == undefined){
+  if (req.query == undefined) {
     return res.status(400).send ({message: 'Peticion de test erronea'})
   } else {
-    res.status(200).send({message:'Email no encontrado en base de datos',exist: false})
+    // Buscamos un email en la tabla users de la base de datos para ver que nos conectamos bien y hacemos un test de conexion con esto
+    let emailURL = "'" + req.query.email + "'"
+    let query = 'SELECT * FROM users WHERE email=' + emailURL
+    conexion.query (query, 
+      function (err, rowCount, rows) {
+        if (err) {
+          throw err
+        } else {
+          if (rowCount.length === 0) {
+            res.status(200).send({message:'Email no encontrado en base de datos',exist: false})
+          } else {
+            res.status(200).send({message:'Email encontrado', exist: true})
+          }
+        }
+      }
+    )
   }   
+}
+
+// User login with response data to front 
+exports.login = (req, res) => {
+  if (req.query == undefined) {
+    return res.status(400).send ({message: 'Login error --> No llegan los datos de la consulta de forma correcta'})
+  }
+  else {
+    let emailURL = "'" + req.query.email + "'"
+    let password = "'" + req.query.email + "'"
+    let queryPassword = 'SELECT * FROM users WHERE email=' + email
+    conexion.query(queryPassword, function (err, rowCount, rows) {
+      if (err) {
+        throw err
+      } else {
+        let passwordQuery = rowCount[0].password
+        // Continuar comparando la clave de la consulta con la clave pasada por la url para poder inciar sesion
+      }
+    })
+  }
 }
