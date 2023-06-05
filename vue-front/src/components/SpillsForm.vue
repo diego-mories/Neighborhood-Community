272ff41ae9910fe11f9369668ebe41484f6b5746
diego mories mayo <diegomoriesmayo@usal.es>
2023-06-05 21:31:02 +0200

    Responsive Derramas

--- vue-front/src/components/SpillsForm.vue
+++ vue-front/src/components/SpillsForm.vue
@@ -1,59 +1,53 @@
 <template>
-    <div>
-      <b-form>
-      <span><img class="w-25 h-25 mw-25 mh-25" src="../assets/images/compra-inmuebles-naranja.png"></span>
-          <b-form-group>
-            <div class="input-group mb-3 align-items-center">
-              <b-form-input
-                v-model="dataForm.amount"
-                id="input-dataForm-amount"
-                name="input-dataForm-amount"
-                v-validate="{ required: true }"
-                type="number"
-                class="form-control mr-3"
-                aria-describedby="input-dataForm-amount-live-feedback"
-                placeholder="Cantidad"
-                :state="validateState('input-dataForm-amount')"
-              ></b-form-input>
-              <b-form-invalid-feedback id="input-dataForm-amount" class="msgE2">
-                {{ veeErrors.first('input-dataForm-amount')?'Campo obligatorio':'' }}
-              </b-form-invalid-feedback>
-              <b-form-input
-                v-model="dataForm.date"
-                id="input-dataForm-date"
-                name="input-dataForm-date"
-                v-validate="{ required: true }"
-                type="month"
-                class="form-control mr-3"
-                aria-describedby="input-dataForm-date-live-feedback"
-                :state="validateState('input-dataForm-date')"
-              ></b-form-input>
-              <b-form-invalid-feedback id="input-dataForm-date" class="msgE2">
-                {{ veeErrors.first('input-dataForm-date')?'Campo obligatorio':'' }}
-              </b-form-invalid-feedback>
-            </div>
-            <div class="input-group mb-3">
-              <label class="label-login">Descripción</label>
-              <b-form-textarea
-                v-model="dataForm.description"
-                id="input-dataForm-description"
-                name="input-dataForm-description"
-                v-validate="{ required: true }"
-                type="text"
-                rows="5"
-                class="form-control mr-3"
-                aria-describedby="input-dataForm-description-live-feedback"
-                :state="validateState('input-dataForm-description')"
-              ></b-form-textarea>
-              <b-form-invalid-feedback id="input-dataForm-description" class="msgE2">
-                {{ veeErrors.first('input-dataForm-description')?'Campo obligatorio':'' }}
-              </b-form-invalid-feedback>
-            </div>
-          </b-form-group>
-          <b-button variant="outline-primary" type="submit" @click.stop.prevent="save()">GUARDAR</b-button>
-      </b-form>
+  <div>
+    <b-form-group>
+      <div class="input-group mb-3">
+        <b-form-input
+          v-model="dataForm.amount"
+          id="input-dataForm-amount"
+          name="input-dataForm-amount"
+          v-validate="{ required: true }"
+          type="number"
+          class="form-control mr-3"
+          aria-describedby="input-dataForm-amount-live-feedback"
+          placeholder="Cantidad"
+          :state="validateState('input-dataForm-amount')"
+        ></b-form-input>
+        <b-form-input
+          v-model="dataForm.date"
+          id="input-dataForm-date"
+          name="input-dataForm-date"
+          v-validate="{ required: true }"
+          type="month"
+          class="form-control mr-3"
+          aria-describedby="input-dataForm-date-live-feedback"
+          :state="validateState('input-dataForm-date')"
+        ></b-form-input>
+      </div>
+      <div class="input-group mb-3">
+        <label class="label-login">Descripción</label>
+        <b-form-textarea
+          v-model="dataForm.description"
+          id="input-dataForm-description"
+          name="input-dataForm-description"
+          v-validate="{ required: true }"
+          type="text"
+          rows="5"
+          class="form-control mr-3"
+          aria-describedby="input-dataForm-description-live-feedback"
+          :state="validateState('input-dataForm-description')"
+        ></b-form-textarea>
+      </div>
+    </b-form-group>
+    <div class="row d-flex flex-grow-1">
+      <div class="col-8 m-auto mt-0">
+        <b-form-group>
+          <b-button class="m-1 custom-button" variant="outline-primary" @click.stop.prevent="save()">GUARDAR</b-button>
+        </b-form-group>
+      </div>
+    </div>
   </div>
-  </template>
+</template>
 <script>
 import BillsSpillsServices from '../services/Bills_Spills'
 export default {
--- vue-front/src/views/Spills.vue
+++ vue-front/src/views/Spills.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="screen">
+    <!-- <div class="screen">
       <div class="row" id="grid-top-log">
           <NavBarPresident id="full"></NavBarPresident>
       </div>
@@ -23,16 +23,51 @@
       <div class="row" id="grid-bottom-home">
         <FooterSocial id="full"></FooterSocial>
       </div>
+    </div> -->
+  <div class="container-fluid bg-svg d-flex flex-column" style="min-height:1329px">
+    <div class="row">
+      <NavBarPresident></NavBarPresident>
     </div>
-    </template>
+    <div class="row" style="margin-top: 120px;">
+      <b-col>
+        <router-link to="/login">
+          <div class="back">
+            <span class="d-none d-lg-block">VOLVER</span>
+            <font-awesome-icon icon="fa-solid fa-tent-arrow-turn-left" style="font-size: 30px; "></font-awesome-icon> 
+          </div>
+        </router-link>
+      </b-col>
+    </div>
+    <div class="row">
+      <b-col>
+        <span class="title"><b>DERRAMAS</b></span>
+      </b-col>
+    </div>
+    <div class="row d-flex mt-5">
+      <div>
+        <span><img height="185" width="200" src="../assets/images/compra-inmuebles-naranja.png"></span>
+      </div>
+    </div>
+    <div class="row flex-grow-1 d-flex">
+        <div class="col-10 col-lg-4 ml-auto mr-auto" style="margin-top: 100px;">
+          <SpillsForm></SpillsForm>
+        </div>
+      </div>
+      <div class="row">
+        <b-col>
+          <FooterSocialNetwork></FooterSocialNetwork>
+        </b-col>
+      </div>
+  </div>
+</template>
 
 <script>
-import FooterSocial from '../components/FooterSocialNetwork.vue'
+import FooterSocialNetwork from '../components/FooterSocialNetwork.vue'
 import SpillsForm from '../components/SpillsForm.vue'
 import NavBarPresident from '../components/NavBarPresident.vue'
 export default {
   components: {
-    FooterSocial,
+    FooterSocialNetwork,
     SpillsForm,
     NavBarPresident
   }
